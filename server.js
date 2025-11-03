a65b6766b47c58e6ce93aaae46b89df2cecd4636
Bielseba <38709883+Bielseba@users.noreply.github.com>
2025-11-03 03:34:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 import authRoutes from './routes/auth.routes.js';
