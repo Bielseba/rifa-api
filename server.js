e3911bc4ce6aa7e7aedb11214c573a14b553bf9e
Bielseba <38709883+Bielseba@users.noreply.github.com>
2025-11-05 03:51:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import ejsMate from "ejs-mate"
-dotenv.config();
-
+import ejsMate from 'ejs-mate';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import adminRoutes from './routes/admin.routes.js';
 import authRoutes from './routes/auth.routes.js';
@@ -14,35 +13,45 @@ import userRoutes from './routes/user.routes.js';
 import purchasesRoutes from './routes/purchases.routes.js';
 import winnersRoutes from './routes/winners.routes.js';
 import webhooksRoutes from './routes/webhooks.routes.js';
-
-import path from 'path';
-import { fileURLToPath } from 'url';
 import adminUiRoutes from './routes/admin.ui.routes.js';
-import { useCookieParser } from './middleware/adminViewAuth.js'; 
+import { useCookieParser } from './middleware/adminViewAuth.js';
+
+dotenv.config();
 
 const app = express();
 
 
-app.use(express.urlencoded({ extended: true }))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.locals.layout = 'admin/layout';
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
 
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.set("view engine", "ejs");
-app.engine("ejs", ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-useCookieParser(app); 
+useCookieParser(app);
+
+
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get('/health', (req, res) =>
-  res.json({ ok: true, now: new Date().toISOString() })
-);
 
-app.use('/admin', adminRoutes);        
+app.get('/health', (req, res) => res.json({ ok: true, now: new Date().toISOString() }));
+
+
+app.use('/admin-ui', adminUiRoutes);
+
+
+app.use('/admin', adminRoutes);
 app.use('/auth', authRoutes);
 app.use('/campaigns', campaignsRoutes);
 app.use('/user', userRoutes);
@@ -50,14 +59,9 @@ app.use('/purchases', purchasesRoutes);
 app.use('/winners', winnersRoutes);
 app.use('/webhooks', webhooksRoutes);
 
-
-app.use('/admin-ui', adminUiRoutes);   
-
 app.use((req, res) => res.status(404).json({ error: 'Not found' }));
 
-
 export default app;
-
 
 if (!process.env.VERCEL) {
   const PORT = process.env.PORT || 4000;
